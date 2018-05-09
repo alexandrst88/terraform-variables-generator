c940233fecc3862aba32b671538cb161bad44cff
Oleksandr Stepanov <alexandrst88@gmail.com>
2018-05-09 15:17:25 +0300

    Added few tests on helpers, utills

--- generator_test.go
+++ generator_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+var mockFile = "mock.tf"
+
+func TestContainsElement(t *testing.T) {
+	testSlice := []string{"Terraform", "Puppet", "Ansible"}
+	if containsElement(testSlice, "Chef") {
+		t.Error("Should return false, but return true")
+	}
+}
+
+func TestGetAllFiles(t *testing.T) {
+	var file, err = os.Create(mockFile)
+	checkError(err)
+	defer file.Close()
+	files, err := getAllFiles(tfFileExt)
+	checkError(err)
+	if len(files) == 0 {
+		t.Error("Should found at least one file")
+	}
+	defer os.Remove(mockFile)
+}
