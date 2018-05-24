d4eb95038e41d7dba8f3e7826595275d0aa72916
Alex Rudd <rudd.alex1@gmail.com>
2018-05-24 14:46:12 +0100

    Detect variables with dashes and numbers in them

--- terraform.go
+++ terraform.go
@@ -11,7 +11,7 @@ type terraformVars struct {
 
 func (t *terraformVars) matchVarPref(row, varPrefix string) {
 	if strings.Contains(row, varPrefix) {
-		pattern := regexp.MustCompile(`var.([a-z?_]+)`)
+		pattern := regexp.MustCompile(`var.([a-z?0-9?_][a-z?0-9?_?-]*)`)
 		match := pattern.FindAllStringSubmatch(row, 1)
 		if len(match) != 0 {
 			res := replacer.Replace(match[0][0])
